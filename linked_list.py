9ffa7481aa02ef44f1f076aae918a1df6fbf1ddf
jul1998 <jguevara321@gmail.com>
2023-01-31 23:32:28 -0600

    New linked list

--- linked_list.py
+++ linked_list.py
@@ -1,86 +1,41 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next_value = None
-
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
 class LinkedList:
-    def __init__(self, value):
-        self.head = {
-            "value": value,
-            "next_value": None
-        }
-        self.tail = self.head  # It is head because there is only 1 element at the beginning
-        self.length = 1
+    def __init__(self):
+        self.head = None
 
-    def append(self, value):
-        newNode = {
-            "value": value,
-            "next_value": None
-        }
-        self.tail["next_value"] = newNode
-        self.tail = newNode
-        self.length += 1
-        return self.head
+    def insert_at_beggining(self, data):
+        node = Node(data, self.head)
+        self.head = node
 
-    def preaprend(self, value):
-        newNode = {"value": value, "next_value": self.head}
+    def print(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        itr = self.head
+        linkedListStr = ""
+        while itr:
+            linkedListStr += str(itr.data) + "-->"
+            itr = itr.next
+        print(linkedListStr)
 
-        self.head = newNode
-        self.length += 1
+    def inset_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
 
-    def print_list(self):
-        newArr = []
-        current_node = self.head
-        while current_node != None:
-            newArr.append(current_node["value"])
-            current_node = current_node["next_value"]
-        return newArr
+        itr = self.head
+        while itr.next:
+            itr = itr.next
 
-    def insert(self, index, value):
-        if index >= self.length:
-            raise IndexError("Index out of range")
-        newNode = {
-            "value": value,
-            "next_value": None
-        }
+        itr.next = Node(data, None)
 
-        leader = self.get_to_index(index - 1)
-        newNode["next_value"] = leader["next_value"]
-        leader["next_value"] = newNode
-        self.length += 1
-
-        if index == self.length - 1:
-            self.tail = newNode
-        return self
-
-    def get_to_index(self, index):
-        counter = 0
-        current_node = self.head
-        while counter != index:
-            current_node = current_node["next_value"]
-            counter += 1
-        return current_node
-
-    def remove(self, index):
-        if index >= self.length:
-            raise IndexError("Index out of range")
-
-        leader = self.get_to_index(index - 1)
-        node_to_delete = leader["next_value"]
-        leader["next_value"] = node_to_delete["next_value"]
-        self.length -= 1
-        return self.print_list()
-
-
-muLinkedList = LinkedList(10)
-
-muLinkedList.append(5)
-muLinkedList.append(16)
-muLinkedList.preaprend(1)
-muLinkedList.preaprend(200)
-muLinkedList.preaprend(84)
-muLinkedList.insert(2, 2)
-print(muLinkedList.print_list())
-muLinkedList.remove(1)
-print(muLinkedList.print_list())
+linkedL = LinkedList()
+linkedL.insert_at_beggining(5)
+linkedL.insert_at_beggining(14)
+linkedL.insert_at_beggining(80)
+linkedL.inset_at_end(20)
+linkedL.print()
