4b4050bd071d81996cf3a59f7a4ed7e71ee603bd
jul1998 <jguevara321@gmail.com>
2023-02-01 00:06:46 -0600

    New linked list

--- linked_list.py
+++ linked_list.py
@@ -30,12 +30,62 @@ class LinkedList:
         itr = self.head
         while itr.next:
             itr = itr.next
-
         itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        #self.head = None
+        for data in data_list:
+            self.inset_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            itr = itr.next
+            count += 1
+        return count
+
+    def remove(self, index):
+        if index < 0 or index> self.get_length():
+            raise IndexError("Index out of range xD")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index -1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index> self.get_length():
+            raise IndexError("Index out of range xD")
+
+        if index == 0:
+            self.insert_at_beggining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = Node(data, itr.next)
+                break
+            itr = itr.next
+            count += 1
 
 linkedL = LinkedList()
 linkedL.insert_at_beggining(5)
 linkedL.insert_at_beggining(14)
 linkedL.insert_at_beggining(80)
 linkedL.inset_at_end(20)
+linkedL.insert_values(["grape", "banana", "orange"])
+#linkedL.remove(1)
+linkedL.insert_at(3,"hahaha")
+print(linkedL.get_length())
 linkedL.print()
